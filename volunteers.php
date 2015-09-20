a25740d3d1b0bd53c470003a5086c911c256ce43
Brock Haugen <brock.haugen@icloud.com>
2015-09-19 23:44:23 -0700

    volunteers wip

--- volunteers.php
+++ volunteers.php
@@ -1,7 +1,7 @@
 <?php
 
 	if (isset($user) && $user->verify()) {
-		$volunteer_info = $db["people"]->select("volunteers","*","wwuid=".$user->wwuid);
+		$volunteer_info = $db["people"]->select("volunteers","*","user_id=".$user->id);
 		if ($volunteer_info[0])
 			$volunteer_info = $volunteer_info[0];
 		else {
@@ -10,12 +10,14 @@
 				"user_id" => $user->id,
 				"wwuid" => $user->wwuid
 			]);
-			$volunteer_info = $db["people"]->select("volunteers","*","wwuid=".$user->wwuid)[0];
+			$volunteer_info = $db["people"]->select("volunteers","*","user_id=".$user->id)[0];
 		}
-		print_r($volunteer_info); die;
 		if (isset($_POST["volunteer_data"])) {
-			
+			echo $_POST["volunteer_data"]); die;
+			$db["people"]->update("volunteers",$_POST["volunteer_data"],"user_id=".$user->id);
+			$volunteer_info = $db["people"]->select("volunteers","*","user_id=".$user->id)[0];
 		}
+		print_r($volunteer_info);
 	}
 
 ?>
