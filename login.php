e8e05725cbf9fae00d1b805299e02b6f76e33f06
Brock Haugen <brock.a.haugen@gmail.com>
2015-11-18 12:42:52 -0800

    login wip

--- login.php
+++ login.php
@@ -7,17 +7,13 @@
 		//$url = "https://webwork.wallawalla.edu/moodle/login/index.php";
 		$senddata = ['username'=>$_POST['username'], 'password'=>$_POST['password']];
 
-		if (strtolower($_POST["username"]) == "joshua.carpio" && md5($_POST["password"]) == "ae71eed4e3e0545eb40db3718206c209") {
-			$return = '<JSON>{"user":{"wwcid":"2002503","username":"Joshua.Carpio","fullname":"Joshua Carpio","status":"Student"}}</JSON>';
-		} else {
-			$ch = curl_init($url);
-			curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query($senddata));
-			curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-			curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
-			curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
-			$return = curl_exec($ch);
-			curl_close($ch);
-		}
+		$ch = curl_init($url);
+		curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query($senddata));
+		curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+		curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
+		curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+		$return = curl_exec($ch);
+		curl_close($ch);
 
 		$user = json_decode(substr($return,6,-7))->user;
 
@@ -32,12 +28,13 @@
 				'status' => $user->status
 			];
 
-			if (!$db["people"]->select("users","id",["wwuid"=>$user->wwuid])) {
-				$parsedUser["id"] = uniqid();
-				$db["people"]->insert("users",$parsedUser);
-			}
-
-			$user = $data["user"] = new loggedInUser($user);
+            $data["user"] = $parsedUser;
+			// if (!$db["people"]->select("users","id",["wwuid"=>$user->wwuid])) {
+			// 	$parsedUser["id"] = uniqid();
+			// 	$db["people"]->insert("users",$parsedUser);
+			// }
+            //
+			// $user = $data["user"] = new loggedInUser($user);
 		} else {
 			$errors[] = "invalid credentials";
 		}
